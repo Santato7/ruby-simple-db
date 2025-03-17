1390cb6634d387847cb88375e88142a7d75090f6
Santato7 <lucas@santato.dev>
2025-03-17 20:48:32 -0300

    feat: add benchmark

---
+++ benchmark.rb
@@ -0,0 +1,37 @@
+require_relative 'main'
+
+if !ARGV[0]
+  puts "ruby benchmark.rb <i>"
+  exit
+end
+
+ALFABETO = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+array_of_keys = []
+array_of_values = []
+
+ARGV[0].to_i.times do |i|
+  key = ''
+  value = ''
+
+  rand(1..15).times do
+    key += ALFABETO[rand(26)]
+  end
+
+  rand(1..15).times do
+    value += ALFABETO[rand(26)]
+  end
+
+  array_of_keys << key
+  array_of_values << value
+end
+
+t1 = Time.now
+
+ARGV[0].to_i.times do |i|
+  write array_of_keys[i], array_of_values[i]
+end
+
+t2 = Time.now
+
+puts "Elapsed time: #{t2 - t1}"
